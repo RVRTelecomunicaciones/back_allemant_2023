7fad0b9651a423bcf17f22189f7192ed21faabbe
Russell <sisalm0202@gmail.com>
2022-09-07 15:34:01 -0500

    Change .Env

--- dist/config/typeorm.config-migrations.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=typeorm.config-migrations.js.map
--- dist/constants/api.auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_AUTH_KEY = void 0;
-exports.API_AUTH_KEY = '#Emergencia911#';
-//# sourceMappingURL=api.auth.js.map
